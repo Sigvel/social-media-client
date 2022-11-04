e8735b6cba5e025701df826b7e36a9f2398a88df
Sigvel <89355439+Sigvel@users.noreply.github.com>
2022-11-04 23:07:03 +0100

    Added localstorage.test.js

---
+++ src/js/storage/test/localstorage.test.js
@@ -0,0 +1,15 @@
+import { save, load } from "../../storage/index.js";
+import { logout } from "../../api/auth/logout";
+
+describe("storage", () => {
+  it("Saves key to localStorage and can retrieves key from localStorage, logout function works correctly", () => {
+    const key = "token";
+    const value = ["nr3ig334go5u9y349"];
+    const stringifiedValue = JSON.stringify(value);
+    save(key, value);
+    expect(load(key)).toEqual(value);
+    expect(localStorage.getItem(key)).toEqual(stringifiedValue);
+    logout();
+    expect(localStorage.getItem(key)).toEqual(null);
+  });
+});
